d3f386e55babfd19d1c3ef4f5ac77bba8550d18e
Vladimir Liankevich <vladimirlyn@gmail.com>
2024-08-08 10:58:37 +0300

    #01: Added new alements Array Task1, map tasks for component UiTodolist

--- src/shared/model/tasks/index.ts
+++ src/shared/model/tasks/index.ts
@@ -5,37 +5,16 @@ export type Task = {
 };
 
 export const task1: Array<Task> = [
-  {
-    id: 1,
-    title: 'HTML&CSS',
-    isDone: true,
-  },
-  {
-    id: 2,
-    title: 'JS',
-    isDone: true,
-  },
-  {
-    id: 3,
-    title: 'React',
-    isDone: false,
-  },
+  { id: 1, title: 'HTML&CSS', isDone: true },
+  { id: 2, title: 'JS', isDone: true },
+  { id: 3, title: 'React', isDone: false },
+  { id: 4, title: 'Redux', isDone: false },
+  { id: 5, title: 'Typescript', isDone: false },
+  { id: 6, title: 'RTK query', isDone: false },
 ];
 
 export const task2: Array<Task> = [
-  {
-    id: 1,
-    title: 'Hello World',
-    isDone: true,
-  },
-  {
-    id: 2,
-    title: 'I am happy',
-    isDone: false,
-  },
-  {
-    id: 3,
-    title: 'Yo',
-    isDone: false,
-  },
+  { id: 1, title: 'Hello World', isDone: true },
+  { id: 2, title: 'I am happy', isDone: false },
+  { id: 3, title: 'Yo', isDone: false },
 ];
